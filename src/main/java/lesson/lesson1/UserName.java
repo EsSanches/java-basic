41395226db692b157bad0affb2e753994a7bd25f
EsSanches <savohin@list.ru>
2025-03-02 17:33:22 +0300

    home work

---
+++ src/main/java/lesson/lesson1/UserName.java
@@ -0,0 +1,55 @@
+package lesson.lesson1;
+
+import java.util.Scanner;
+
+class UserName {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // String str = scanner.nextLine();
+        // System.out.println("Hello " + str);
+
+//        System.out.print("Введите число: ");
+//        int num = scanner.nextInt();
+//
+//        int res1 = num * 1;
+//        System.out.println(num + " * 1 = " + res1);
+//
+//        int res2 = num * 2;
+//        System.out.println(num + " * 2 = " + res2);
+
+
+//        System.out.print("Введите радиус: ");
+//        int num = scanner.nextInt();
+//
+//        double pi = Math.PI;
+//
+//        double res = 2 * pi * num;
+//        System.out.println("Длина окружности " + res);
+//
+//        double res2 = Math.pow(num, 2) * pi;
+//        System.out.println("Площадь окружности " + res2);
+
+
+//        System.out.print("Введите числа: ");
+//        int num1 = scanner.nextInt();
+//        int num2 = scanner.nextInt();
+//        int num3 = scanner.nextInt();
+//
+//
+//        double res = (num1 + num2 + num3) / 3;
+//        System.out.print("среднее арифметическое " + res);
+
+
+
+
+
+
+
+
+
+
+    }
+}
