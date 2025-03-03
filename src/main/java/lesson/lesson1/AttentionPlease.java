f4a863958f31258821841bf33a1b5cd862c4cd9d
EsSanches <savohin@list.ru>
2025-03-03 19:35:03 +0300

    home work

---
+++ src/main/java/lesson/lesson1/AttentionPlease.java
@@ -0,0 +1,35 @@
+package lesson.lesson1;
+
+class AttentionPlease {
+
+    public static void main(String[] args) {
+
+//        int numberOne = 10;
+//        int numberTwo = 20;
+//
+//        System.out.println("Number1 = " + numberOne);
+//        System.out.println("Number1 = " + numberTwo);
+//
+//        int sum = numberOne + numberTwo;
+//        System.out.println("Sum = " + sum);
+
+        int numberOne = 10;
+        double numberTwo = 20.0;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        // однострочный ком
+        /* многострочные
+            ком
+         */
+
+        /** докумен-
+         * тировавние
+         */
+        double sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
