84f17bd9611381af565ac588897de9bb5fc7d0ab
EsSanches <savohin@list.ru>
2025-03-07 11:59:27 +0300

    home work

---
+++ src/main/java/lesson/lesson2/HW/RobotApp.java
@@ -0,0 +1,15 @@
+package lesson.lesson2.HW;
+
+class RobotApp {
+    public static void main(String[] args) {
+
+        Robot robot1 = new Robot("Rex");
+
+        robot1.setName("Mark");
+        System.out.println(robot1.getName());
+
+
+
+    }
+
+}
