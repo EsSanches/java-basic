fe99a40578c87d1a08da6066e40ff4457bf7960f
EsSanches <savohin@list.ru>
2025-03-10 12:00:41 +0300

    home work

---
+++ src/main/java/lesson/lesson2/HW/task2/Car.java
@@ -0,0 +1,40 @@
+package lesson.lesson2.HW.task2;
+
+
+class Car {
+    private String type;
+
+    private String color;
+
+    private int price;
+
+    public Car(String type, String color, int price) {
+        this.type = type;
+        this.color = color;
+        this.price = price;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
